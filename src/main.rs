64662af30946f9a71054a750f8a684a313c9fbd8
Robert O'Shea <robertoshea2k11@gmail.com>
2023-12-02 19:34:35 +0000

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,6 @@ fn main() {
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
     commands.spawn(SpriteBundle {
-        texture: asset_server.load("icon.png"),
         ..Default::default()
     });
 }
