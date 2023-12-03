71163d944cba168dadf015d4acb87d82ca32b299
Robert O'Shea <robertoshea2k11@gmail.com>
2023-12-03 18:48:59 +0000

    Temp push

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,16 @@
 // Feel free to delete this line.
 #![allow(clippy::too_many_arguments, clippy::type_complexity)]
 
+mod animation;
 mod enemy;
 mod player;
 mod state;
 
+use animation::AnimationIndicies;
 use bevy::{asset::AssetMetaCheck, prelude::*, window::WindowTheme};
 use enemy::EnemySpawnPlugin;
 use player::PlayerPlugin;
 use state::GameState;
-
-#[derive(Component)]
-struct AnimationIndicies {
-    first: usize,
-    last: usize,
-}
 
 #[derive(Component, Deref, DerefMut)]
 struct AnimationTimer(Timer);
@@ -59,7 +55,7 @@ fn setup(
     asset_server: Res<AssetServer>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
-    let texture_handle = asset_server.load("demon_walk.png");
+    let texture_handle = asset_server.load("sprites/demon_walk.png");
     let texture_atlas = TextureAtlas::from_grid(
         texture_handle,
         Vec2::new(12., 16.),
