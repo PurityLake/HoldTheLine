772138a7b83a425fc5cdac2aab24b959770031af
Robert O'Shea <robertoshea2k11@gmail.com>
2023-12-11 02:53:51 +0000

    Add new states to game

--- src/state.rs
+++ src/state.rs
@@ -1,8 +1,11 @@
 use bevy::prelude::*;
 
+#[allow(dead_code)]
 #[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
 pub enum GameState {
     #[default]
     MainMenu,
+    TransitionToGamePlay,
     GamePlay,
+    Pause,
 }
